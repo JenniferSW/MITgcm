6cab7de8cdf32ebbe9d464fa607a8d054f29d238
JenniferSW <jwesselbaum@marum.de>
2020-06-02 13:26:56 +0200

    deleted all empty variables

--- pkg/iceberg/ICEBERG.h
+++ pkg/iceberg/ICEBERG.h
@@ -1,58 +1,41 @@
 #ifdef ALLOW_ICEBERG
 
 C     Package flag
-      LOGICAL myPa_MNC
-      LOGICAL myPa_MDSIO
-      COMMON /MYPA_ICEBERG/
-     &                     myPa_MNC, myPa_MDSIO
+c      LOGICAL myPa_MNC
+c      LOGICAL myPa_MDSIO
+c      COMMON /ICEBERG_PACKAGE/
+c     &                     myPa_MNC, myPa_MDSIO
 
 
 
 C     ICEBERG parameters
-      LOGICAL myPa_StaV_Cgrid
-      LOGICAL myPa_Tend_Cgrid
-      LOGICAL myPa_applyTendT
-      LOGICAL myPa_applyTendS
-      LOGICAL myPa_applyTendU
-      LOGICAL myPa_applyTendV
+
+
 
 C-    additional parameters:
 c     ICEBERG_ITER0   :: iteration number to start iceberg drift
 c     ICEBERG_numClUsed :: Number of icebergs used in the simulation
 c     ICEBERG_ClLbl   :: Labels for diagnostics output
 
-      LOGICAL myPa_doSwitch1
-      LOGICAL myPa_doSwitch2
       INTEGER ICEBERG_Iter0
-      INTEGER myPa_index2
       INTEGER ICEBERG_numClUsed
-      _RL myPa_param1
-      _RL myPa_param2
       CHARACTER*(8) ICEBERG_ClLbl(ICEBERG_numCl)
-      CHARACTER*(MAX_LEN_FNAM) myPa_string2
 
 C-    file names for initial conditions:
 C     ICEBERG_initialFile   :: File with initial conditions for icebergs
 
       CHARACTER*(MAX_LEN_FNAM) ICEBERG_initialFile(ICEBERG_numCl)
-      CHARACTER*(MAX_LEN_FNAM) myPa_Scal2File
-      CHARACTER*(MAX_LEN_FNAM) myPa_VelUFile
-      CHARACTER*(MAX_LEN_FNAM) myPa_VelVFile
-      CHARACTER*(MAX_LEN_FNAM) myPa_Surf1File
-      CHARACTER*(MAX_LEN_FNAM) myPa_Surf2File
 
-      COMMON /MYPA_PARAMS_L/
-     &       myPa_StaV_Cgrid, myPa_Tend_Cgrid,
-     &       myPa_applyTendT, myPa_applyTendS,
-     &       myPa_applyTendU, myPa_applyTendV,
-     &       myPa_doSwitch1, myPa_doSwitch2
-      COMMON /MYPA_PARAMS_I/ ICEBERG_Iter0, myPa_index2,
+C- Common Block for Logical Parameter
+c      COMMON /ICEBERG_PARAMS_L/
+c     &       myPa_StaV_Cgrid, myPa_Tend_Cgrid,
+
+C- Common Block for Integer, Real and Character
+      COMMON /ICEBERG_PARAMS_I/ ICEBERG_Iter0,
      &       ICEBERG_numClUsed
-      COMMON /MYPA_PARAMS_R/ myPa_param1, myPa_param2
-      COMMON /MYPA_PARAMS_C/ ICEBERG_ClLbl, myPa_string2,
-     &       ICEBERG_initialFile, myPa_Scal2File,
-     &       myPa_VelUFile,  myPa_VelVFile,
-     &       myPa_Surf1File, myPa_Surf2File
+c      COMMON /ICEBERG_PARAMS_R/
+      COMMON /ICEBERG_PARAMS_C/ ICEBERG_ClLbl,
+     &       ICEBERG_initialFile
 
 #ifdef ICEBERG_3D_STATE
 C     ICEBERG (3D) fields
@@ -60,29 +43,24 @@ C     iceberg_distr    :: field for iceberg distribution
 
       _RL iceberg_distr(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,
      &  	      nSy,ICEBERG_numCl)
-      _RL myPa_StatScal2(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL myPa_StatVelU(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL myPa_StatVelV(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      COMMON /MYPA_STATE_3D/
-     &    iceberg_distr, myPa_StatScal2,
-     &    myPa_StatVelU,  myPa_StatVelV
+
+C- Common Block for 3D fields
+      COMMON /ICEBERG_STATE_3D/
+     &    iceberg_distr
+
 #endif /* ICEBERG_3D_STATE */
 #ifdef ICEBERG_2D_STATE
-C     MYPA 2-dim. fields
-      _RL myPa_Surf1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      _RL myPa_Surf2(1-OLx:sNx+OLx,1-OLy:sNy+OLy,nSx,nSy)
-      COMMON /MYPA_STATE_2D/
-     &    myPa_Surf1, myPa_Surf2
+C     Iceberg 2-dim. fields
+
+c      COMMON /ICEBERG_STATE_2D/
+
 #endif /* ICEBERG_2D_STATE */
 
 #ifdef ICEBERG_TENDENCY
-      _RL myPa_TendScal1(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL myPa_TendScal2(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL myPa_TendVelU(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      _RL myPa_TendVelV(1-OLx:sNx+OLx,1-OLy:sNy+OLy,Nr,nSx,nSy)
-      COMMON /MYPA_TENDENCY/
-     &    myPa_TendScal1, mypa_TendScal2,
-     &    myPa_TendVelU,  mypa_TendVelV
+c Tendency fields
+
+C      COMMON /ICEBERG_TENDENCY/
+
 #endif /* ICEBERG_TENDENCY */
 
 #endif /* ALLOW_ICEBERG */
